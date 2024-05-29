744a5d60d7e5a35b3d50e1f675792a711e18e8eb
GenosseK <akazhanov72@yandex.ru>
2024-05-29 14:17:20 +0500

    Updated exe file and deleted test videos

--- dist/app.js
+++ dist/app.js
@@ -8,15 +8,30 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.App = void 0;
 const ffmpeg_executor_js_1 = require("./commands/ffmpeg/ffmpeg.executor.js");
 const console_logger_js_1 = require("./out/console-logger/console.logger.js");
+const readline_1 = __importDefault(require("readline"));
 class App {
     run() {
         return __awaiter(this, void 0, void 0, function* () {
             const executor = new ffmpeg_executor_js_1.FfmpegExecutor(console_logger_js_1.ConsoleLogger.getInstance());
             yield executor.execute();
+            this.waitForExit();
+        });
+    }
+    waitForExit() {
+        const rl = readline_1.default.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+        rl.question('Press Enter to exit...', () => {
+            rl.close();
+            process.exit(0);
         });
     }
 }
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,24 @@
 import { FfmpegExecutor } from "./commands/ffmpeg/ffmpeg.executor.js";
 import { ConsoleLogger } from "./out/console-logger/console.logger.js";
+import readline from 'readline';
 
 export class App {
     async run() {
         const executor = new FfmpegExecutor(ConsoleLogger.getInstance());
         await executor.execute();
+        this.waitForExit();
+    }
+
+    waitForExit() {
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+
+        rl.question('Press Enter to exit...', () => {
+            rl.close();
+            process.exit(0);
+        });
     }
 }
 
